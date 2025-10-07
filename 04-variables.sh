275e77c1c0a777f4f015fce53f76a6b9c4e40a12
guruprasad <guruprasad3889@gmail.com>
2025-10-07 17:52:24 -0400

    adding files

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Welcome to Alaska $PERSON2"
+echo "$PERSON2:: Thank yoou $PERSON1, finally not on Zoom webinar"
+echo "$PERSON1:: So, do we have a deal"
+esho "$PERSON2:: Yes, if you make Ukraine joins Russia"
